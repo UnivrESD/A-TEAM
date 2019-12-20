ad6bfda3433b410bcf67c16165a4b1bd9cb870b3
SamueleGerminiani <samuele.germiniani@univr.it>
2019-12-20 21:24:44 +0100

    Cmake Hotfix findAntrl

--- cmake/FindANTLR.cmake
+++ cmake/FindANTLR.cmake
@@ -1,10 +1,17 @@
 include(FindPackageHandleStandardArgs)
 
 find_path(ANTLR4CPP_INCLUDE_DIRS
-    PATHS ./libs/antlr4/runtime/Cpp/runtime/src
+    PATHS ./libs/lib/antlr4/runtime/Cpp/runtime/src
     NAMES antlr4-runtime.h
     DOC "ANTLR4 C++ headers"
     )
+if (NOT ANTLR4CPP_INCLUDE_DIRS)
+    find_path(ANTLR4CPP_INCLUDE_DIRS
+        PATHS ./libs/antlr4/runtime/Cpp/runtime/src
+        NAMES antlr4-runtime.h
+        DOC "ANTLR4 C++ headers"
+        )
+endif ()
 
 if (NOT ANTLR4CPP_INCLUDE_DIRS)
     message(STATUS "Could not find C++ ANTLR include path")
